8d208095679cd8bafa11457caccfd146b1842450
Vanshaj Bhatnagar <vanshajbhatnagar10@gmail.com>
2023-07-03 19:55:44 -0400

    updated server.js file.

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,9 @@ const db = require("./config/db");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const studentRoutes = require("./routes/studentRoutes");
-// import mongoose from "mongoose";
-// import pkg from "./config/config.js";
-// const { MONGODB_URI } = pkg;
 
 const app = express();
 const PORT = 8000;
-
-// // Connect to MongoDB
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
 
 app.use(express.json());
 
